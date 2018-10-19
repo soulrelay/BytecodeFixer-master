6e2fb2506476fcdb320da9920311e0f93c3223e8
sushuai <sushuai@didichuxing.com>
2018-10-19 10:44:33 +0800

    在做一些简单的小测试 发现类似constraint-layout-solver-1.1.0是可以再injector中生成fixedclasses 但是通过遍历发现却有好多classes.jar 有点疑惑

--- app/src/main/java/com/llew/bytecode/fix/MainActivity.java
+++ app/src/main/java/com/llew/bytecode/fix/MainActivity.java
@@ -1,9 +1,12 @@
 package com.llew.bytecode.fix;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
